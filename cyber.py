e7663fea14e1c0971a01def373a014bc3aa6d88f
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2019-11-27 22:52:09 -0300

    atualizando denovo

--- cyber.py
+++ cyber.py
@@ -1,20 +1,42 @@
 import RPi.GPIO as GPIO
+# import leitor_audio
+import calcula
 import time
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO23
-GPIO.setup(24, GPIO.OUT)  #LED to GPIO24
+GPIO.setup(24, GPIO.OUT)   #LED to GPIO24
 
-try:
-    while True:
-         button_state = GPIO.input(23)
-         if button_state == False:
-             GPIO.output(24, True)
-             print('Button Pressed...')
-             time.sleep(0.2)
-         else:
-             GPIO.output(24, False)
 
-except:
-    GPIO.cleanup()
+def loop_principal():
+    nomearq = "result.wav"
+    last_state = False
+    inicial = time.time()
+    C = 0
+    try:
+        while True:
+            button_state = GPIO.input(23) == GPIO.HIGH
+            if button_state == False:
+                if last_state == True:
+                    tempo = time.time() - inicial
+                    GPIO.output(24, True)
+                    time.sleep(tempo)
+                    GPIO.output(24, False)
+                    # leitor_audio.gravar(nomearq, record_time=tempo)
+                    calcula.calcular_db(nomearq)
+
+                print('{C} Button Pressed...'.format({C:C}))
+                time.sleep(0.2)
+            else:
+                if last_state == False:
+                   inicial = time.time()
+                # GPIO.output(24, False)
+            last_state = button_state
+
+    except Exception as e:
+        print(e)
+        GPIO.cleanup()
+
+
+loop_principal()
--- hardware_functions.py
+++ hardware_functions.py
@@ -1,1 +1,7 @@
 #  funções que vão simular o data input do arduino
+
+import time
+
+t = time.time()
+time.sleep(2)
+print(int(time.time() - t))
